5a531600001f9614c9a7f62d39b1f386444737c7
Nika <nikadvalishvili517@gmail.com>
2024-07-06 16:49:48 +0400

    navbar starting point

--- portfolio/src/components/Navbar/index.jsx
+++ portfolio/src/components/Navbar/index.jsx
@@ -1,21 +1,32 @@
 import React from "react";
-import { ReactTyped } from "react-typed";
+// import { ReactTyped } from "react-typed";
+import "../Navbar/style.css"
 const Navbar = () => {
-  const [value, setValue] = React.useState("one");
+  // const [value, setValue] = React.useState("one");
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  // const handleChange = (event, newValue) => {
+  //   setValue(newValue);
+  // };
   return (
     <>
-       <ReactTyped
+       {/* <ReactTyped
           strings={["Hi! I am Nika Dvalishvili"]}
           typeSpeed={100}
           backSpeed={20}
           cursorChar=">"
           showCursor={true}
           style={{fontFamily: 'retro', color: "#17D102", fontSize: "25px"}}
-        />
+        /> */}
+
+        <div className="navbar_content">
+          <div className="inner_content">
+            <a href="" className="home">Home</a>
+            <a href="" className="projects">Projects</a>
+            <a href="" className="about">About</a>
+            <a href="" className="contact">Contact</a>
+          </div>
+        </div>
+        
     </>
   );
 };
