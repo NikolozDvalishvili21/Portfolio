71f890e519fad273c61dce8c8a207085ceda24d4
Nika <nikadvalishvili517@gmail.com>
2024-07-06 18:56:09 +0400

    made navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Home from "./pages/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "flowbite/dist/flowbite.css";
+
 
 function App() {
   return <>
--- portfolio/src/components/Navbar/index.jsx
+++ portfolio/src/components/Navbar/index.jsx
@@ -1,32 +1,69 @@
-import React from "react";
-// import { ReactTyped } from "react-typed";
-import "../Navbar/style.css"
-const Navbar = () => {
-  // const [value, setValue] = React.useState("one");
+import React, { useState } from "react";
+import "../Navbar/style.css";
+import useIsMobile from "../../hooks/resize";
 
-  // const handleChange = (event, newValue) => {
-  //   setValue(newValue);
-  // };
+const Navbar = () => {
+  const isMobile = useIsMobile();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
-       {/* <ReactTyped
-          strings={["Hi! I am Nika Dvalishvili"]}
-          typeSpeed={100}
-          backSpeed={20}
-          cursorChar=">"
-          showCursor={true}
-          style={{fontFamily: 'retro', color: "#17D102", fontSize: "25px"}}
-        /> */}
-
-        <div className="navbar_content">
+      {isMobile ? (
+        <div className={`burger ${isOpen ? "hide" : ""}`} onClick={toggleMenu}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+      ) : (
+        <div className={`navbar_content ${isOpen ? "open" : ""}`}>
           <div className="inner_content">
-            <a href="" className="home">Home</a>
-            <a href="" className="projects">Projects</a>
-            <a href="" className="about">About</a>
-            <a href="" className="contact">Contact</a>
+            <a href="#" className="home">
+              Home
+            </a>
+            <a href="#" className="projects">
+              Projects
+            </a>
+            <a href="#" className="about">
+              About
+            </a>
+            <a href="#" className="contact">
+              Contact
+            </a>
           </div>
         </div>
-        
+      )}
+
+      {isMobile && isOpen && (
+        <div className="overlay">
+          <div className="sidebar">
+            <div className="inner_content_side">
+              <a href="#" className="home">
+                Home
+              </a>
+              <a href="#" className="projects">
+                Projects
+              </a>
+              <a href="#" className="about">
+                About
+              </a>
+              <a href="#" className="contact">
+                Contact
+              </a>
+            </div>
+            <div className="close-btn" onClick={closeMenu}>
+              X
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
